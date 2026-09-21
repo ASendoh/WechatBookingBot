cf10c584305e82a7a278b4e282b38b79c6b247da
ASendoh <328323114@qq.com>
2026-09-21 10:09:47 +0800

    选第二天前增加0.1秒等待

--- select_tomorrow_test.py
+++ select_tomorrow_test.py
@@ -8,7 +8,7 @@ def select_tomorrow() -> None:
     x = 438
     y = 187
 
-    # 保留两次点击，以及两次点击之间的 0.1 秒间隔。
+    time.sleep(0.1)
     pyautogui.click(x, y)
     time.sleep(0.1)
     pyautogui.click(x, y)
